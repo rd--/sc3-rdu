d1786f8d35c1846c600ff659ae28c89377255ce6
Rohan Drape <rd@slavepianos.org>
2007-12-04 11:20:04 +0000

    Update scheme help file

--- scheme/RDelayMap.help.scm
+++ scheme/RDelayMap.help.scm
@@ -1,4 +1,4 @@
-;; UGen definit-1n
+;; UGen definition
 
 (define-filter* RDelayMap (bufnum in dyn specArray) 1)
 
@@ -8,13 +8,13 @@
 
 ;; Simple delay (static)
 
-(let* ((x (MouseX kr 110 440 0 0.1))
-       (y (MouseY kr 0.0 0.2 0 0.1))
-       (s (Mul (SinOsc ar x 0) y)))
-  (Mce (RDelayMap 0 s 0
-		  (Mce -1 0 0 0.5
-		       1 -1 1 1))
-       s))
+(audition
+ (let* ((x (MouseX kr 110 440 0 0.1))
+	(y (MouseY kr 0.0 0.2 0 0.1))
+	(s (Mul (SinOsc ar x 0) y))
+	(d (Mce -1 0 0 0.5
+		1 -1 1 1)))
+   (Out 0 (Mce (RDelayMap 0 s 0 d) s))))
 
 ;; Simple feedback circuit (static)
 
@@ -24,20 +24,22 @@
 
 (->< s (/b_zero 0))
 
-(RDelayMap 0 (pulse 5/4) 0
-	   (Mce -1  0  0 1
-		1/9 0  1  8/9
-		1   -1 0 1))
+(audition
+ (let ((d (Mce -1  0  0 1
+	       1/9 0  1  8/9
+	       1   -1 0 1)))
+   (Out 0 (RDelayMap 0 (pulse 5/4) 0 d))))
 
 ;; Simple feedback circuit (dynamic)
 
 (->< s (/b_zero 0))
 
-(let ((x (MouseX kr 0.05 1.05 0 0.1))
-      (y (MouseY kr 0.05 0.95 0 0.1)))
-  (RDelayMap 0 (pulse (Mce 1/3 5/4)) 1
-	     (Mce -1 0  0 1
-		  y  0  1  x
-		  1  -1 0 1)))
+(audition
+ (let* ((x (MouseX kr 0.05 1.05 0 0.1))
+	(y (MouseY kr 0.05 0.95 0 0.1))
+	(d (Mce -1 0 0 1
+		y  0 1 x
+		1 -1 0 1)))
+   (Out 0 (RDelayMap 0 (pulse (Mce 1/3 5/4)) 1 d))))
 
 (->< s (/b_free 0))
