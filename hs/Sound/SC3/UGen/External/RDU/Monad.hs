636ea386256991fb9e5e4f632eda559b675173b1
Rohan Drape <rd@slavepianos.org>
2013-03-23 23:16:49 +0000

    ~hsc3(liftU -> liftUId)

--- hs/Sound/SC3/UGen/External/RDU/Monad.hs
+++ hs/Sound/SC3/UGen/External/RDU/Monad.hs
@@ -3,19 +3,18 @@ module Sound.SC3.UGen.External.RDU.Monad where
 
 import Sound.SC3.UGen.Rate
 import Sound.SC3.UGen.Type
-import Sound.SC3.UGen.UGen.Lift
 import Sound.SC3.UGen.UId
 
 import qualified Sound.SC3.UGen.External.RDU.ID as I
 
 dustR :: UId m => Rate -> UGen -> UGen -> m UGen
-dustR = liftU3 I.dustR
+dustR = liftUId3 I.dustR
 
 expRandN :: UId m => Int -> UGen -> UGen -> m UGen
-expRandN nc = liftU2 (I.expRandN nc)
+expRandN nc = liftUId2 (I.expRandN nc)
 
 randN :: UId m => Int -> UGen -> UGen -> m UGen
-randN nc = liftU2 (I.randN nc)
+randN nc = liftUId2 (I.randN nc)
 
 tScramble :: UId m => Rate -> UGen -> UGen -> m UGen
-tScramble = liftU3 I.tScramble
+tScramble = liftUId3 I.tScramble
