9a0171bbc0a7c5951925a36ea62922ea859850d5
Rohan Drape <rd@slavepianos.org>
2014-08-10 02:47:40 +0000

    ~hsc3/hsc3-db

--- hs/Sound/SC3/UGen/External/RDU.hs
+++ hs/Sound/SC3/UGen/External/RDU.hs
@@ -2,7 +2,7 @@
 module Sound.SC3.UGen.External.RDU where
 
 import Sound.SC3.UGen {- hsc3 -}
-import Sound.SC3.UGen.UGen.Construct {- hsc3 -}
+import Sound.SC3.UGen.Bindings.HW.Construct {- hsc3 -}
 import Sound.SC3.UGen.DB.Record {- hsc3-db -}
 
 std_I :: Int -> String -> Double -> I
@@ -241,6 +241,44 @@ tScramble_dsc =
          ,ugen_pseudo_inputs = Nothing
          ,ugen_fixed_rate = Just KR
          }
+
+-- dustR :: ID z => z -> Rate -> UGen -> UGen -> UGen
+-- dustR z rt lo hi = mkOscId (toUId z) rt "DustR" [lo,hi] 1
+
+expRandN :: ID z => Int -> z -> UGen -> UGen -> UGen
+expRandN nc z l r = mkOscId (toUId z) IR "ExpRandN" [l,r] nc
+
+iRandN :: ID z => Int -> z -> UGen -> UGen -> UGen
+iRandN nc z l r = mkOscId (toUId z) IR "IRandN" [l,r] nc
+
+linRandN :: ID z => Int -> z -> UGen -> UGen -> UGen -> UGen
+linRandN nc z l r bias = mkOscId (toUId z) IR "LinRandN" [l,r,bias] nc
+
+randN :: ID z => Int -> z -> UGen -> UGen -> UGen
+randN nc z l r = mkOscId (toUId z) IR "RandN" [l,r] nc
+
+tScramble :: ID z => z -> Rate -> UGen -> UGen -> UGen
+tScramble z rt tr i = mkOscMCEId (toUId z) rt "TScramble" [tr] i (mceDegree i)
+
+-- * Monadic
+
+-- dustRM :: UId m => Rate -> UGen -> UGen -> m UGen
+-- dustRM = liftUId3 dustR
+
+expRandNM :: UId m => Int -> UGen -> UGen -> m UGen
+expRandNM nc = liftUId2 (expRandN nc)
+
+iRandNM :: UId m => Int -> UGen -> UGen -> m UGen
+iRandNM nc = liftUId2 (iRandN nc)
+
+linRandNM :: UId m => Int -> UGen -> UGen -> UGen -> m UGen
+linRandNM nc = liftUId3 (linRandN nc)
+
+randNM :: UId m => Int -> UGen -> UGen -> m UGen
+randNM nc = liftUId2 (randN nc)
+
+tScrambleM :: UId m => Rate -> UGen -> UGen -> m UGen
+tScrambleM = liftUId3 tScramble
 
 -- Local Variables:
 -- truncate-lines:t
