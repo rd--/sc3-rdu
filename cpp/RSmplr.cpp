9f68d40480f63492787ed89fb688f81bded7b212
rd <rd@slavepianos.org>
2021-01-02 07:19:12 +0000

    +RSmpr,+TExpRand,initial

---
+++ cpp/RSmplr.cpp
@@ -0,0 +1,68 @@
+#include <stdio.h>
+
+#include <SC_PlugIn.h>
+
+#include "rdu.h"
+
+static InterfaceTable *ft;
+
+/* RSmplrIndex */
+
+struct RSmplrIndex : public Unit
+{
+    rdu_declare_buf(tbl);
+    float m_mnn;
+    float m_index;
+    float m_mul;
+};
+
+rdu_prototypes(RSmplrIndex);
+
+void RSmplrIndex_Ctor(RSmplrIndex* unit)
+{
+    rdu_init_buf(tbl);
+    unit->m_mnn = 0.0;
+    unit->m_index = 0.0;
+    unit->m_mul = 1.0;
+    SETCALC(RSmplrIndex_next);
+    RSmplrIndex_next(unit, 1);
+}
+
+float midi_to_ratio(float mnn)
+{
+    return powf(2.0,mnn * (1.0 / 12.0));
+}
+
+float midi_diff_to_ratio(float mnn0,float mnn1)
+{
+    return midi_to_ratio(mnn1 - mnn0);
+}
+
+void RSmplrIndex_next(RSmplrIndex* unit, int inNumSamples)
+{
+    rdu_get_buf(tbl,0);
+    rdu_check_buf(tbl,1);
+    int tbl_n = (int) IN0(1);
+    float mnn = IN0(2);
+    int index = 0;
+    if (mnn != unit->m_mnn) {
+        for(index = 0; index < tbl_n - 1 ; index++) {
+            if(mnn >= unit->m_buf_tbl->data[index] && mnn < unit->m_buf_tbl->data[index + 1]) {
+                break;
+            }
+        }
+        unit->m_mnn = mnn;
+        unit->m_index = (float)index;
+        unit->m_mul = midi_diff_to_ratio(mnn,unit->m_buf_tbl->data[index]);
+    }
+    OUT0(0) = unit->m_index;
+    OUT0(1) = unit->m_mul;
+}
+
+/* LOAD */
+
+PluginLoad(RSmplr)
+{
+  ft = inTable;
+  DefineSimpleUnit(RSmplrIndex);
+}
--- cpp/RandN.cpp
+++ cpp/RandN.cpp
@@ -2,6 +2,8 @@
 
 static InterfaceTable *ft;
 
+/* ExpRandN */
+
 struct ExpRandN : public Unit {};
 
 void ExpRandN_Ctor(ExpRandN* unit)
@@ -17,6 +19,8 @@ void ExpRandN_Ctor(ExpRandN* unit)
   }
 }
 
+/* IRandN */
+
 struct IRandN : public Unit {};
 
 void IRandN_Ctor(IRandN* unit)
@@ -31,6 +35,8 @@ void IRandN_Ctor(IRandN* unit)
     OUT0(i) = (float)(rgen.irand(range) + lo);
   }
 }
+
+/* LinRandN */
 
 struct LinRandN : public Unit {};
 
@@ -55,6 +61,8 @@ void LinRandN_Ctor(LinRandN* unit)
   }
 }
 
+/* RandN */
+
 struct RandN : public Unit {};
 
 void RandN_Ctor(RandN* unit)
@@ -69,6 +77,8 @@ void RandN_Ctor(RandN* unit)
     OUT0(i) = rgen.frand() * d + l;
   }
 }
+
+/* TRandN */
 
 struct TRandN : public Unit {float m_trig, *m_store;};
 
@@ -119,6 +129,59 @@ void TRandN_Dtor(TRandN *unit)
     RTFree(unit->mWorld,unit->m_store);
 }
 
+/* TExpRandN */
+
+struct TExpRandN : public Unit {float m_trig, *m_store;};
+
+void TExpRandN_gen(TExpRandN* unit)
+{
+    uint32 nc = unit->mNumOutputs;
+    float lo = IN0(0);
+    float hi = IN0(1);
+    float z = hi / lo;
+    RGen& rgen = *unit->mParent->mRGen;
+    uint32 i;
+    for (i=0;i<nc;i++) {
+	unit->m_store[i] = pow(z,rgen.frand()) * lo;
+    }
+}
+
+void TExpRandN_cpy(TExpRandN* unit)
+{
+    uint32 nc = unit->mNumOutputs;
+    uint32 i;
+    for (i=0;i<nc;i++) {
+	OUT0(i) = unit->m_store[i];
+    }
+}
+
+void TExpRandN_next_k(TExpRandN* unit, int inNumSamples)
+{
+    float trig = IN0(2);
+    if (trig > 0.f && unit->m_trig <= 0.f) {
+	TExpRandN_gen(unit);
+    }
+    TExpRandN_cpy(unit);
+    unit->m_trig = trig;
+}
+
+void TExpRandN_Ctor(TExpRandN* unit)
+{
+    unit->m_store = (float*)RTAlloc(unit->mWorld,unit->mNumOutputs * sizeof(float));
+    TExpRandN_gen(unit);
+    TExpRandN_cpy(unit);
+    SETCALC(TExpRandN_next_k);
+    unit->m_trig = IN0(2);
+    TExpRandN_next_k(unit, 1);
+}
+
+void TExpRandN_Dtor(TExpRandN *unit)
+{
+    RTFree(unit->mWorld,unit->m_store);
+}
+
+/* LOAD */
+
 PluginLoad(RandN)
 {
   ft = inTable;
@@ -126,5 +189,6 @@ PluginLoad(RandN)
   DefineSimpleUnit(IRandN);
   DefineSimpleUnit(LinRandN);
   DefineSimpleUnit(RandN);
+  DefineDtorUnit(TExpRandN);
   DefineDtorUnit(TRandN);
 }
