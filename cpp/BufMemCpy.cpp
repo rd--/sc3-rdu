5396d2896a8ab3e497cbbc3e294d703e225e3998
rohan <rd@slavepianos.org>
2023-10-30 20:12:17 +1100

    ~BufMemCpy,+byteSwap

--- cpp/BufMemCpy.cpp
+++ cpp/BufMemCpy.cpp
@@ -5,7 +5,14 @@
 
 static InterfaceTable *ft;
 
-// Arguments are: 1. numFrames:int, 2. numChannels:int, 3, sampleRate:float, 4. bufferData:blob (copied directly into the buffer).
+/*
+Arguments are:
+1. numFrames:int,
+2. numChannels:int,
+3, sampleRate:float,
+4. bufferData:blob (copied directly into the buffer)
+5. byteSwap:int{0,4}
+*/
 void BufMemCpy(World *world, struct SndBuf *buf, struct sc_msg_iter *msg)
 {
     int bufSize = buf->samples * sizeof(float);
@@ -21,12 +28,15 @@ void BufMemCpy(World *world, struct SndBuf *buf, struct sc_msg_iter *msg)
         buf->samples = numSamples;
         buf->frames = numFrames;
         msg->getb((char *)(buf->data), dataSize);
-        for (int i = 0; i < numSamples; i++) {
-            u32 nextValue;
-            u8 *nextAddress = (u8 *)(buf->data) + (i * 4);
-            memcpy(&nextValue, nextAddress, 4);
-            ntoh32_to_buf(nextAddress, nextValue);
-        }
+	int byteSwap = msg->geti(0); /* default value = 0 */
+	if(byteSwap == 4) {
+	    for (int i = 0; i < numSamples; i++) {
+		u32 nextValue;
+		u8 *nextAddress = (u8 *)(buf->data) + (i * 4);
+		memcpy(&nextValue, nextAddress, 4);
+		ntoh32_to_buf(nextAddress, nextValue);
+	    }
+	}
     } else {
         Print("BufMemCpy.memcpy: illegal input data for buffer, data size and buffer size must be equal");
     }
