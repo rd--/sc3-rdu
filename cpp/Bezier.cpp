95335f66dfc828a6ca872760e51d2110dfe24c78
rohan <rd@slavepianos.org>
2024-01-05 17:58:19 +1100

    bexier,failedexperiments

--- cpp/Bezier.cpp
+++ cpp/Bezier.cpp
@@ -1,12 +1,16 @@
 #include <assert.h>
 #include <stdio.h>
 
-#include <SC_PlugIn.h>
+// #define DEBUG
 
+#include <SC_PlugIn.h>
 #include "r-common/c/bezier.c"
 #include "r-common/c/print.h"
 
 #include "rdu.hpp"
+
+// #define BezierMaxPt 16
+// #define BezierMaxCtl (BezierMaxPt * 6 - 4)
 
 static InterfaceTable *ft;
 
@@ -17,32 +21,53 @@ struct Bezier : public Unit {
 	int m_points;
 };
 
+// #define GetCtl(k) getCtl[pt + k](i)
+// #define GetCtl(k) getInput(unit, pt + 4 + k, i)
+#define GetCtl(k) IN0(pt + 4 + k)
+
 /* k dx fr ph {x0 y0} cx0 cy0 cx1 cy1 x1 y1 ... */
 void Bezier_next(Bezier *unit, int inNumSamples)
 {
 	float *out = OUT(0);
+	/*
+	if(unit->m_points > BezierMaxPt) {
+		printf("Bezier: exceeds maximum allowed points: %d > %d\n",
+		       unit->m_points, BezierMaxPt);
+		for (int i = 0; i < inNumSamples; i++) {
+			out[i] = 0.0;
+		}
+		return;
+	}
+	*/
 	int halt_after = unit->m_halt_after;
 	float dx = unit->m_dx;
 	float ph = unit->m_phase;
 	float ph_incr = IN0(2) / unit->mRate->mSampleRate;
 	assert(ph_incr > 0.0 && ph_incr <= 1.0);
+	/*
+	GetInput getCtl[BezierMaxCtl];
+	for (uint32 k = 0; k < unit->mNumInputs - 4; k++) {
+		getCtl[k] = genGet(unit, k + 4);
+	}
+	*/
 	for (int i = 0; i < inNumSamples; i++) {
 		bool coherent = false;
 		out[i] = 0.0;
 		assert(ph >= 0.0 && ph <= 1.0);
-		for (int j = 0; j < unit->m_points; j++) {
-			int pt = 4 + (j * 6);
-			float x0 = getInput(unit, pt + 0, i);
-			float x3 = getInput(unit, pt + 6, i);
+		for (int j = 0; j < unit->m_points - 1; j++) {
+			int pt = j * 6;
+			float x0 = GetCtl(0);
+			float x3 = GetCtl(6);
 			dprintf("i=%i, j=%i, pt=%d, ph=%.3f, x0=%.3f, x3=%.3f\n",
 				i, j, pt, ph, x0, x3);
 			if (ph >= x0 && ph <= x3) {
-				float y0 = getInput(unit, pt + 1, i);
-				float x1 = getInput(unit, pt + 2, i);
-				float y1 = getInput(unit, pt + 3, i);
-				float x2 = getInput(unit, pt + 4, i);
-				float y2 = getInput(unit, pt + 5, i);
-				float y3 = getInput(unit, pt + 7, i);
+				dprintf("%.3f <= %.3f <= %.3f\n", x0, ph, x3);
+				float y0 = GetCtl(1);
+				float x1 = GetCtl(2);
+				float y1 = GetCtl(3);
+				float x2 = GetCtl(4);
+				float y2 = GetCtl(5);
+				float y3 = GetCtl(7);
 				dprintf("(%.3f,.3%f) (%.3f,%.3f) (%.3f,%.3f) (%.3f,%.3f)\n",
 					x0, y0, x1, y1, x2, y2, x3, y3);
 				out[i] = bezier4_y_mt(halt_after, dx,
@@ -70,7 +95,7 @@ void Bezier_Ctor(Bezier *unit)
 	unit->m_dx = IN0(1);
 	unit->m_phase = IN0(3);
 	unit->m_points = 1 + ((unit->mNumInputs - 4) / 6);
-	dprintf("Bezier: initial-phase = %0.3f, # points = %d\n",
+	dprintf("Bezier: initial-phase = %0.3f, #points = %d\n",
 		unit->m_phase, unit->m_points);
 	SETCALC(Bezier_next);
 	assert(IN0(3) >= 0.0 && IN0(3) <= 1.0);
